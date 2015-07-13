fb6d00300728626032a5fed3fa4be4443a894912
Andrew Lodes <alodes999@gmail.com>
2015-07-13 14:07:55 -0500

    Add new views for route in home.erb
    Move layout to correct place

--- controllers/home.rb
+++ controllers/home.rb
@@ -2,3 +2,18 @@ get "/home" do
   erb :"home/home"
 end
 
+get "/assignment" do
+  erb :"home/assignment"
+end
+
+get "/classmate" do
+  erb :"home/classmate"
+end
+
+get "/resource" do
+  erb :"home/resource"
+end
+
+get "/contribution" do
+  erb :"home/contribution"
+end
