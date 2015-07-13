a8fbf32bdd3f2692ae0392c4d348ccdd7aa93f41
Andrew Lodes <alodes999@gmail.com>
2015-07-13 14:22:28 -0500

    Add base views pages for each views folder.
    Add basic controller functionality for resource.erb/resource_controller.rb

---
+++ controllers/resource_controller.rb
@@ -0,0 +1,16 @@
+get "resource/view" do
+  erb :"resource/view"
+end
+
+get "resource/add" do
+  erb :"resource/add"
+end
+
+get "resource/edit" do
+  erb :"resource/edit"
+end
+
+get "resource/delete" do
+  erb :"resource/delete"
+end
+  
