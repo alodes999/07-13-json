47005c93c24fe6e63b302df2960c076c10e630a8
Andrew Lodes <alodes999@gmail.com>
2015-07-13 12:50:26 -0500

    Finish database setup for project
    Rename controllers/models for better self-recognition

--- database_setup.rb
+++ database_setup.rb
@@ -1,9 +1,8 @@
-CONNECTION = SQLite3::Database.new("gamereviewrepos.db")
+CONNECTION = SQLite3::Database.new("jsonproject.db")
 
-CONNECTION.execute("CREATE TABLE IF NOT EXISTS genres (id INTEGER PRIMARY KEY, name TEXT);")
-CONNECTION.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT);")
-CONNECTION.execute("CREATE TABLE IF NOT EXISTS formats (id INTEGER PRIMARY KEY, name TEXT);")
-CONNECTION.execute("CREATE TABLE IF NOT EXISTS games (id INTEGER PRIMARY KEY, name TEXT, genres_id INTEGER, formats_id INTEGER, FOREIGN KEY(genres_id) REFERENCES genres(id), FOREIGN KEY(formats_id) REFERENCES formats(id) );")
-CONNECTION.execute("CREATE TABLE IF NOT EXISTS reviews (id INTEGER PRIMARY KEY, games_id INTEGER, users_id INTEGER, score INTEGER, FOREIGN KEY(games_id) REFERENCES games(id), FOREIGN KEY(users_id) REFERENCES users(id) );")
+CONNECTION.execute("CREATE TABLE IF NOT EXISTS assignments (id INTEGER PRIMARY KEY, description TEXT, github text);")
+CONNECTION.execute("CREATE TABLE IF NOT EXISTS classmates (id INTEGER PRIMARY KEY, name TEXT);")
+CONNECTION.execute("CREATE TABLE IF NOT EXISTS resources (id INTEGER PRIMARY KEY, link TEXT, assignments_id INTEGER, FOREIGN KEY(assignments_id) REFERENCES assignments(id) );")
+CONNECTION.execute("CREATE TABLE IF NOT EXISTS contributions (assignments_id INTEGER, classmates_id INTEGER, FOREIGN KEY(assignments_id) REFERENCES assignments(id), FOREIGN KEY(classmates_id) REFERENCES classmates(id) );")
 
 CONNECTION.results_as_hash = true
